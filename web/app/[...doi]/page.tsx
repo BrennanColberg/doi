a0faa8ee5fa5bd2ca7ff4c8f2a33600b0c080131
Brennan Colberg <brennan.colberg@gmail.com>
2024-01-01 14:51:32 -0800

    iframe use client

--- web/app/[...doi]/page.tsx
+++ web/app/[...doi]/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation"
+import DOIFrame from "./DOIFrame"
 
 export default async function DoiPage({ params }: { params: { doi: string[] } }) {
   // verify the DOI and find info about it
@@ -15,14 +16,7 @@ export default async function DoiPage({ params }: { params: { doi: string[] } })
     <main className="w-screen h-screen flex flex-col justify-start p-5 gap-5">
       {/* iframe the actual site for readability */}
       {/* TODO add button to go to actual site? */}
-      <iframe
-        className="flex-grow border-black border shadow-lg"
-        src={`https://doi.org/${doi}`}
-        // TODO maybe check for 404 in an `onload` here instead?
-        //      as all invalid DOIs at `doi.org` show the same page
-        onError={(error) => console.error("onError", error)}
-        onErrorCapture={(error) => console.error("onErrorCapture", error)}
-      />
+      <DOIFrame doi={doi} />
       <pre>{JSON.stringify(params, null, 2)}</pre>
     </main>
   )
