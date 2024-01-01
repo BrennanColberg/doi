1c0069904e577fe06beec58f2ea432be1f2ff268
Brennan Colberg <brennan.colberg@gmail.com>
2024-01-01 15:43:28 -0800

    better examples

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,14 +2,20 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <main>
+    <main className="w-screen p-5">
       <h1>DOI chat</h1>
-      <ul>
+      <ul className="list-disc list-inside">
         <li>
-          example:{" "}
           <Link href="/10.1000/182" className="text-blue-500 underline">
-            doi handbook
-          </Link>
+            example
+          </Link>{" "}
+          with working iframe
+        </li>
+        <li>
+          <Link href="/10.1093/ajae/aaq063" className="text-blue-500 underline">
+            example
+          </Link>{" "}
+          where iframe is disabled by the site
         </li>
       </ul>
     </main>
