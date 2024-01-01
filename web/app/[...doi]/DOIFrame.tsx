38393fadf38b1595b3d00fc70d446f22746be413
Brennan Colberg <brennan.colberg@gmail.com>
2024-01-01 15:40:13 -0800

    iframeable checker

--- web/app/[...doi]/DOIFrame.tsx
+++ web/app/[...doi]/DOIFrame.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-export default function DOIFrame({ doi, href }: { doi: string; href?: string }) {
+export default function DOIFrame({ href }: { href?: string }) {
   // upgrade links to https so that they don't break the iframe when served from https
   // note: this will screw up sites that are legitimately http-only. but they'd be broken
   // anyway as-is, so this is a reasonable tradeoff
@@ -9,13 +9,10 @@ export default function DOIFrame({ doi, href }: { doi: string; href?: string }) 
     href = href.replace("http://", "https://")
   }
 
-  if (href) console.log("loading iframe from href", href)
-  else console.log("loading iframe from DOI", doi)
-
   return (
     <iframe
       className="flex-grow border-black border shadow-lg"
-      src={href ?? `https://doi.org/${doi}`}
+      src={href}
       // TODO maybe check for 404 in an `onload` here instead?
       //      as all invalid DOIs at `doi.org` show the same page
       onError={(error) => console.error("onError", error)}
--- web/app/[...doi]/page.tsx
+++ web/app/[...doi]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation"
 import DOIFrame from "./DOIFrame"
+import Link from "next/link"
 
 export default async function DoiPage({ params }: { params: { doi: string[] } }) {
   // verify the DOI and find info about it
@@ -7,23 +8,50 @@ export default async function DoiPage({ params }: { params: { doi: string[] } })
   // check if DOI is facially valid
   if (!doi.startsWith("10.")) notFound()
   // if it looks decent see if it's real
-  const response = await fetch(`https://doi.org/${doi}`, { redirect: "manual" })
-  if (response.status === 404) notFound()
+  const doiResponse = await fetch(`https://doi.org/${doi}`, { redirect: "manual" })
+  if (doiResponse.status === 404) notFound()
+
   // if given a 302 back, extract the redirect URL and load it directly
   // in the iframe (this also allows for manual protocol upgrades to HTTPS)
   let href: string | undefined
-  if (response.status === 302) {
-    const text = await response.text()
+  if (doiResponse.status === 302) {
+    const text = await doiResponse.text()
     const match = text.match(/https?:\/\/[^"]*/)
     href = match?.[0]
   }
+  if (!href) {
+    console.error(`unexpected status ${doiResponse.status} from doi.org/${doi}`)
+    notFound() // TODO handle responses that aren't 404/302
+  }
+
+  // check if the href is iframeable
+  let iframeable = true
+  const siteResponse = await fetch(href)
+  const frameOptions = siteResponse.headers.get("X-Frame-Options")
+  if (frameOptions?.toLowerCase().includes("deny")) iframeable = false
+  if (frameOptions?.toLowerCase().includes("sameorigin")) iframeable = false
+  const csp = siteResponse.headers.get("Content-Security-Policy")
+  if (csp?.toLowerCase().includes("frame-ancestors 'none'")) iframeable = false
+  // note: in theory they could specifically allow us, but rn that's unlikely
+  //       so for now we just assume *any* restriction blocks our iframes
+  if (csp?.toLowerCase().includes("frame-ancestors")) iframeable = false
 
   return (
     <main className="w-screen h-screen flex flex-col justify-start p-5 gap-5">
       {/* iframe the actual site for readability */}
       {/* TODO add button to go to actual site? */}
-      <DOIFrame doi={doi} href={href} />
-      <pre>{JSON.stringify(params, null, 2)}</pre>
+      {iframeable ? (
+        <DOIFrame href={href} />
+      ) : (
+        <p className="text-red-500">
+          Unable to preview this site. Please{" "}
+          <Link href={href} className="underline">
+            visit it directly
+          </Link>
+          .
+        </p>
+      )}
+      <pre>{JSON.stringify({ doi }, null, 2)}</pre>
     </main>
   )
 }
